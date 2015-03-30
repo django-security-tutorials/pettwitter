0dcf2dcba07c1a1a78dbddc578aea4010639a644
Asheesh Laroia <asheesh@asheesh.org>
2015-03-29 20:38:59 -0700

    Heroku-ify, for database connection

--- thesite/thesite/settings.py
+++ thesite/thesite/settings.py
@@ -70,6 +70,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+import os
+import dj_database_url
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
