c25da0a57d38f3f9d05a3e428dd3fff3cf4ea6ed
Karen Rustad <karen.rustad@gmail.com>
2015-03-30 23:52:55 -0700

    Bootstrappification styling changes, and some prose.

--- thesite/communication_app/forms.py
+++ thesite/communication_app/forms.py
@@ -7,7 +7,17 @@ class PetForm(forms.ModelForm):
         model = communication_app.models.Pet
         fields = ['name']
 
+    def __init__(self, *args, **kwargs):
+        super(PetForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget = forms.TextInput(attrs={
+            'class': 'form-control'})
+
 class UpdateForm(forms.ModelForm):
     class Meta:
         model = communication_app.models.Update
         fields = ['text']
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateForm, self).__init__(*args, **kwargs)
+        self.fields['text'].widget = forms.TextInput(attrs={
+            'class': 'form-control'})
