b272d7af7f513f593fe1af828798820cee321f08
Asheesh Laroia <asheesh@asheesh.org>
2015-03-30 22:50:12 -0700

    Always install dj-database-url

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ import os
 extra_depends = []
 if 'DYNO' in os.environ:
     extra_depends.append('psycopg2==2.6')
-    extra_depends.append('dj-database-url==0.3.0')
 # end dependency trickery.
 
 setup(
