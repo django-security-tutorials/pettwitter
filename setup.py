8bc62353a74003d176c385aba5b7d9f32e6036c7
Asheesh Laroia <asheesh@asheesh.org>
2015-03-30 22:46:11 -0700

    Move psycopg to Heroku-only list of dependencies

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import os
 # trying to have different dependencies on Heroku.
 extra_depends = []
 if 'DYNO' in os.environ:
+    extra_depends.append('psycopg2==2.6')
     extra_depends.append('dj-database-url==0.3.0')
 # end dependency trickery.
 
