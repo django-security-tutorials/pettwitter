9198c686b9ed8a02d32a63490743bbdbd57d66ce
Asheesh Laroia <asheesh@asheesh.org>
2015-03-30 22:36:12 -0700

    Explain this trickery in a comment

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from codecs import open
 from setuptools import setup
 import os
 
+# Note to security researchers -- this trickery here is really
+# not part of a security vulnerability. It's just the app maintainers
+# trying to have different dependencies on Heroku.
 extra_depends = []
 if 'DYNO' in os.environ:
     extra_depends.append('dj-database-url==0.3.0')
+# end dependency trickery.
 
 setup(
     name='thesite',
