9d026534b382aa9ff74aed8a9a06ed19b7ee4ab6
Jacinda Shelly <jacinda.shelly@gmail.com>
2018-10-11 18:46:38 -0700

    Migrate over create_user, login and logout urls

--- thesite/base_app/urls.py
+++ thesite/base_app/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.site_index, name="index")]
+urlpatterns = [
+    path("", views.site_index, name="index"),
+    path("create_user/", views.create_user, name="create_user"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+]
--- thesite/thesite/urls.py
+++ thesite/thesite/urls.py
@@ -1,16 +1,12 @@
 from django.urls import include, path
 from django.contrib import admin
 
-urlpatterns = [path("", include("base_app.urls"))]
+urlpatterns = [
+    # For everything generic, look in base_app
+    path("", include("base_app.urls"))
+]
 
 # urlpatterns = patterns(
-#     '',  # First arg to patterns is a namespace parameter.
-#
-#     # A handful of views in a base app.
-#     url(r'^$', 'base_app.views.site_index'),
-#     url(r'^logout/$', 'base_app.views.logout'),
-#     url(r'^create_user/$', 'base_app.views.create_user'),
-#     url(r'^login/$', 'base_app.views.login'),
 #
 #     # Actual pet communication views -- most of the app is here.
 #     url(r'^pets/', include('communication_app.urls')),
