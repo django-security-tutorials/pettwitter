3c62871c8db8faddd3910b03492873b43c796895
Jacinda Shelly <jacinda.shelly@gmail.com>
2018-10-12 14:40:29 -0700

    Fix url path

--- thesite/communication_app/urls.py
+++ thesite/communication_app/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path("new_pet/", views.new_pet, name="new_pet"),
     path("profiles/<int:pet_id>/", views.profile, name="profile"),
     path("update/<pet_id>/", views.update, name="update"),
-    path("set_description/<int:pet_id>", views.set_description, name="set_description"),
+    path(
+        "set_description/<int:pet_id>/", views.set_description, name="set_description"
+    ),
     path("delete_my_pets/", views.delete_my_pets, name="delete_my_pets"),
 ]
