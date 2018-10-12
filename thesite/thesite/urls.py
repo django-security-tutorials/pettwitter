b47bdf57729e7b6ccc873b09c07963ddfef355ee
Jacinda Shelly <jacinda.shelly@gmail.com>
2018-10-12 14:34:33 -0700

    Blackify

--- thesite/thesite/urls.py
+++ thesite/thesite/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path("", include("base_app.urls")),
     # For pet communication views, prefix with pets. This contains the bulk of this
     # project's application logic.
-    path('pets/', include('communication_app.urls', namespace='pets')),
+    path("pets/", include("communication_app.urls", namespace="pets")),
     # Include the views from Asheesh's Django Optimizer
     # TODO: Update asheeshs_django_optimizer for modern Django
     # path('optimizer/', include('asheeshs_django_optimizer.urls')),
     # Django admin.
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
