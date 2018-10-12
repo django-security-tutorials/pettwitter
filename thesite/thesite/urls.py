c9c3f3a10bc0e449c5b4da9025cc0ad6e496fa2f
Jacinda Shelly <jacinda.shelly@gmail.com>
2018-10-12 13:33:57 -0700

    Update main site urls

--- thesite/thesite/urls.py
+++ thesite/thesite/urls.py
@@ -1,5 +1,6 @@
-from django.urls import include, path
 from django.contrib import admin
+from django.urls import include, path
+
 
 urlpatterns = [
     # For everything generic, look in base_app
@@ -7,16 +8,9 @@ urlpatterns = [
     # For pet communication views, prefix with pets. This contains the bulk of this
     # project's application logic.
     path('pets/', include('communication_app.urls', namespace='pets')),
+    # Include the views from Asheesh's Django Optimizer
+    # TODO: Update asheeshs_django_optimizer for modern Django
+    # path('optimizer/', include('asheeshs_django_optimizer.urls')),
+    # Django admin.
+    path('admin/', admin.site.urls),
 ]
-
-# urlpatterns = patterns(
-#
-#     # Actual pet communication views -- most of the app is here.
-#     url(r'^pets/', include('communication_app.urls')),
-#
-#     # Include the views from Asheesh's Django Optimizer
-#     url(r'^optimizer/', include('asheeshs_django_optimizer.urls')),
-#
-#     # Django admin.
-#     url(r'^admin/', include(admin.site.urls)),
-# )
