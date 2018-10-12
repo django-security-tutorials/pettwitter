1f4c47a718bfcdc10e36f3e2b93d1184d234714c
Jacinda Shelly <jacinda.shelly@gmail.com>
2018-10-12 13:53:11 -0700

    Blackify

--- thesite/communication_app/urls.py
+++ thesite/communication_app/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 
 from . import views
 
-app_name = 'pets'
+app_name = "pets"
 urlpatterns = [
     path("", views.index, name="index"),
     # TODO: Verify paths below this comment
     path("new_pet/", views.new_pet, name="new_pet"),
     path("profiles/<int:pet_id>/", views.profile, name="profile"),
-    path("update/<>/", views.update, name="update"),
-    path("set_description/<int>", views.set_description, name="set_description"),
+    path("update/<pet_id>/", views.update, name="update"),
+    path("set_description/<int:pet_id>", views.set_description, name="set_description"),
     path("delete_my_pets/", views.delete_my_pets, name="delete_my_pets"),
 ]
