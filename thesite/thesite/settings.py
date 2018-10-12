34e06baf6a61d6d5e78d58a97a4448e70a8d3134
Jacinda Shelly <jacinda.shelly@gmail.com>
2018-10-11 18:25:32 -0700

    Add TEMPLATES setting; site index now loading\!

--- thesite/thesite/settings.py
+++ thesite/thesite/settings.py
@@ -95,3 +95,20 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, ".static-generated")
+
+# https://docs.djangoproject.com/en/2.1/topics/templates/
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
