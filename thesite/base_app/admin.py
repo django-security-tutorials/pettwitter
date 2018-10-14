5606577f39ead0efe47d94cebfddce330b06c152
Jacinda Shelly <jacinda.shelly@gmail.com>
2018-10-14 02:06:40 -0700

    Add some extra fields to the UserAdmin

--- thesite/base_app/admin.py
+++ thesite/base_app/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+# Override default list_display for the UserAdmin
+UserAdmin.list_display = (
+    "pk",
+    "username",
+    "password",
+    "email",
+    "first_name",
+    "last_name",
+    "is_staff",
+)
