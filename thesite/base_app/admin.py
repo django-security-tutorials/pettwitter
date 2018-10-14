b40cbd94ceeb565deaed61dac2f4354b6397c351
Jacinda Shelly <jacinda.shelly@gmail.com>
2018-10-14 03:04:48 -0700

    Update comments

--- thesite/base_app/admin.py
+++ thesite/base_app/admin.py
@@ -2,6 +2,8 @@ from django.contrib.auth.admin import UserAdmin
 
 
 # Override default list_display for the UserAdmin
+# Including password in the list of fields displayed should be safe because all that's stored there is a hashed
+# value, so no actual user passwords will be exposed
 UserAdmin.list_display = (
     "pk",
     "username",
